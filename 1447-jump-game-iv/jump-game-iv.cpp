551efbbc1ff5210e4c0f087da71649c31ff54a1e
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-05-18 21:43:57 +0530

    Time: 131 ms (37.62%) | Memory: 98.9 MB (32.95%) - LeetSync

---
+++ 1447-jump-game-iv/jump-game-iv.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        if(n == 1) return 0;
+
+        unordered_map<int,vector<int>> pos;
+        for(int i = 0; i<n; i++) {
+            pos[arr[i]].push_back(i);
+        }
+
+        vector<bool> vis (n,false);
+        int cnt = 0;
+
+        queue<int> q;
+        q.push(0);
+        vis[0] = true;
+
+        while(!q.empty()) {
+            int size = q.size();
+            while(size--) {
+                int idx = q.front();
+                q.pop();
+
+                if(idx == n-1) return cnt;
+                if(idx-1 >= 0 && !vis[idx-1]) {
+                    q.push(idx-1);
+                    vis[idx-1] = true;
+                }
+                if(idx+1 >= 0 && !vis[idx+1]) {
+                    q.push(idx+1);
+                    vis[idx+1] = true;
+                }
+                for(int &val : pos[arr[idx]]) {
+                    if(!vis[val]) {
+                        q.push(val);
+                        vis[val] = true;
+                    }
+                }
+                if(pos.find(arr[idx]) != pos.end()) {
+                    pos.erase(arr[idx]);
+                }
+            }
+            cnt++;
+        }
+
+        return -1;
+    }
+};
