cfa9ed36a6f8f55d5c04ce4f1a8a11102d6499c2
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-06-03 22:41:51 +0530

    Time: 0 ms (100.00%) | Memory: 238.1 MB (42.20%) - LeetSync

--- 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
+++ 3967-earliest-finish-time-for-land-and-water-rides-ii/earliest-finish-time-for-land-and-water-rides-ii.cpp
@@ -7,29 +7,31 @@ public:
         int land = INT_MAX;
         int water = INT_MAX; 
 
-        for(int i = 0; i < max(n,m); i++) {
-            if(i < n) land = min(land,landS[i] + landD[i]);
-            if(i < m) water = min(water,waterS[i] + waterD[i]);
+        for(int i = 0; i < n; i++) {
+            land = min(land,landS[i] + landD[i]);
+        }
+
+        for(int i = 0; i < m; i++) {
+            water = min(water,waterS[i] + waterD[i]);
         }
 
         int ans = INT_MAX;
 
-        for(int i = 0; i < max(m,n); i++) {
-            if(i < m) {
-                if(waterS[i] >= land) {
-                    ans = min(ans,waterS[i] + waterD[i]);
-                } else {
-                    ans = min(ans,land + waterD[i]);
-                }   
+        for(int i = 0; i < n; i++) {
+            if(landS[i] >= water) {
+                ans = min(ans,landS[i] + landD[i]);
+            } else {
+                ans = min(ans,water + landD[i]);
             }
+        
+        }
 
-            if(i < n) {
-                if(landS[i] >= water) {
-                    ans = min(ans,landS[i] + landD[i]);
-                } else {
-                    ans = min(ans,water + landD[i]);
-                }
-            }
+        for(int i = 0; i < m; i++) {
+            if(waterS[i] >= land) {
+                ans = min(ans,waterS[i] + waterD[i]);
+            } else {
+                ans = min(ans,land + waterD[i]);
+            }  
         }
 
        return ans;
