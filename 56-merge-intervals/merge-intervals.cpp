27a3efcefd71c54b3ee9078c4cb4591c671ff406
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-06-10 15:06:07 +0530

    Time: 11 ms (12.64%) | Memory: 23.8 MB (58.85%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -6,20 +6,18 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> res;
 
-        int st = intervals[0][0];
-        int en = intervals[0][1];
+        for(int i = 0; i<n; i++) {
+            int st = intervals[i][0];
+            int en = intervals[i][1];
 
-        for(int i = 1; i<n; i++) {
-            if(intervals[i][0] <= en) {
+            while(i < n && intervals[i][0] <= en) {
                 en = max(en,intervals[i][1]);
-            } else {
-                res.push_back({st,en});
-                st = intervals[i][0];
-                en = intervals[i][1];
+                i++;
             }
-        }
 
-        res.push_back({st,en});
+            i--;
+            res.push_back({st,en});
+        }
 
         return res;
     }
