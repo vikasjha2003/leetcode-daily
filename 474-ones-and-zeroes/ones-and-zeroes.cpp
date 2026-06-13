68393c57e6e2493fb91a9c234f77c8e7531f2931
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-06-13 10:33:26 +0530

    Time: 223 ms (16.98%) | Memory: 107.6 MB (7.49%) - LeetSync

--- 474-ones-and-zeroes/ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/ones-and-zeroes.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> count (string & str) {
+    vector<int> count (string& str) {
         int one = 0;
         int zero = 0;
 
@@ -15,30 +15,30 @@ public:
         return {zero,one};
     }
 
-    int solve (vector<vector<int>> &arr, int m, int n, int idx, vector<vector<vector<int>>> &dp) {
-        if(idx == arr.size()) return 0;
-        if(dp[idx][m][n] != -1) return dp[idx][m][n];
-
-        int skip = solve(arr,m,n,idx+1,dp);
-
-        int pick = 0;
-
-        if(arr[idx][0] <= m && arr[idx][1] <= n) {
-            pick = 1 + solve(arr,m - arr[idx][0], n - arr[idx][1], idx + 1, dp);
-        }
-
-        return dp[idx][m][n] = max(skip , pick);
-    }
-
     int findMaxForm(vector<string>& strs, int m, int n) {
-        vector<vector<int>> arr;
+        int s = strs.size();
 
-        for(int i = 0; i<strs.size(); i++) {
+        vector<vector<int>> arr;
+        for(int i = 0; i<s; i++) {
             arr.push_back(count(strs[i]));
         }
 
-        vector<vector<vector<int>>> dp (arr.size(), vector<vector<int>> (m+1, vector<int> (n+1, -1)));
+        vector<vector<vector<int>>> dp (s+1, vector<vector<int>> (m+1, vector<int> (n+1, 0)));
 
-        return solve(arr,m,n,0,dp);
+        for(int i = s-1; i >= 0; i--) {
+            for(int j = m; j >= 0; j--) {
+                for(int k = n; k >= 0; k--) {
+                    int skip = dp[i+1][j][k];
+                    int pick = 0;
+                    if(arr[i][0] <= j && arr[i][1] <= k) {
+                        pick = 1 + dp[i+1][j-arr[i][0]][k-arr[i][1]];
+                    }
+
+                    dp[i][j][k] = max(skip,pick);
+                }
+            }
+        }
+
+        return dp[0][m][n];
     }
 };
