e2f804a4578c10720bcf8cd4c9e64e9ede22b2f9
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-07-15 11:15:25 +0530

    Time: 443 ms (71.30%) | Memory: 290.5 MB (46.09%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -4,6 +4,8 @@ public:
     const int mod = 1e9 + 7;
 
     int gcd (int a , int b) {
+        if(a == 0) return b;
+        
         while(b != 0) {
             int temp = b;
             b = a%b;
@@ -17,17 +19,11 @@ public:
             if(g1 == g2 && g1 != 0) return 1;
             return 0;
         }
-
         if(dp[idx][g1][g2] != -1) return dp[idx][g1][g2];
 
         int skip = subcount(nums,idx+1,g1,g2,dp);
-
-        int take1 = 0;
-        if(g1 != 0) take1 += subcount(nums,idx+1,gcd(g1,nums[idx]),g2,dp);
-        else take1 += subcount(nums,idx+1,nums[idx],g2,dp);
-        int take2 = 0;
-        if(g2 != 0) take2 += subcount(nums,idx+1,g1,gcd(g2,nums[idx]),dp);
-        else take2 += subcount(nums,idx+1,g1,nums[idx],dp);
+        int take1 = subcount(nums,idx+1,gcd(g1,nums[idx]),g2,dp);
+        int take2 = subcount(nums,idx+1,g1,gcd(g2,nums[idx]),dp);
 
         return dp[idx][g1][g2] = ((skip + take1) % mod + take2) % mod;
     }
@@ -35,7 +31,7 @@ public:
     int subsequencePairCount(vector<int>& nums) {
         n = nums.size();
         int maxi = *max_element(nums.begin(), nums.end());
-        vector<vector<vector<int>>> dp (n, vector<vector<int>> (maxi +1 , vector<int> (maxi +1,-1)));
+        vector<vector<vector<int>>> dp (n, vector<vector<int>> (maxi+1 , vector<int> (maxi+1,-1)));
         return subcount(nums,0,0,0,dp);
     }
 };
