1765cc6df75b145952d59b110b75b3c24eca7bdf
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-07-30 13:20:31 +0530

    Time: 3 ms (55.54%) | Memory: 24.2 MB (38.85%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        // Horizontal
+        for(int i = 0; i<9; i++) {
+            unordered_set<int> st;
+            for(int j = 0; j<9; j++) {
+                if(board[i][j] == '.') continue;
+                if(st.find(board[i][j]) != st.end()) return false;
+                st.insert(board[i][j]);
+            }
+        }
+
+        // vertical
+        for(int i = 0; i<9; i++) {
+            unordered_set<int> st;
+            for(int j = 0; j<9; j++) {
+                if(board[j][i] == '.') continue;
+                if(st.find(board[j][i]) != st.end()) return false;
+                st.insert(board[j][i]);
+            }
+        }
+
+        // box
+        for(int i = 0; i<9; i+=3) {
+            for(int j = 0; j<9; j+=3) {
+                unordered_set<int> st;
+                for(int row = i; row < i + 3; row++) {
+                    for(int col = j; col < j + 3; col++) {
+                        if(board[row][col] == '.') continue;
+                        if(st.find(board[row][col]) != st.end()) return false;
+                        st.insert(board[row][col]);
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+};
