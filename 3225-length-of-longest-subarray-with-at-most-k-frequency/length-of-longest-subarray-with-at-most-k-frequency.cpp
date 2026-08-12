224e65bf46f320bf25ca8bf14bc40c2d70ce70d2
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-08-12 19:08:44 +0530

    Time: 90 ms (12.36%) | Memory: 151.1 MB (8.76%) - LeetSync

--- 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 3225-length-of-longest-subarray-with-at-most-k-frequency/length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -4,20 +4,17 @@ public:
         int n = nums.size();
 
         unordered_map<int,int> freq;
-        int l = 0, r = 0;
+        int l = 0;
         int maxLen = 0;
 
-        while(r < n) {
+        for(int r = 0; r < n; r++) {
             freq[nums[r]]++;
-            
             while(freq[nums[r]] > k) {
                 freq[nums[l]]--;
                 if(freq[nums[l]] == 0) freq.erase(nums[l]);
                 l++;
             }
-
             maxLen = max(maxLen, r-l+1);
-            r++;
         }
 
         return maxLen;
