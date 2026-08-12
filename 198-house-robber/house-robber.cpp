429f3180b3944ed15c383b585e7875697b175f45
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-08-12 20:53:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (24.22%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
+    int n;
+    int solve(vector<int>& nums, int idx, vector<int>& dp) {
+        if(idx >= n) return 0;
+        if(dp[idx] != -1) return dp[idx];
+
+        int take = nums[idx] + solve(nums,idx+2,dp);
+        int skip = solve(nums,idx+1,dp);
+
+        return dp[idx] = max(skip,take);
+    }
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return nums[0];
-        int prev2 = 0; // second previous house
-        int prev1 = nums[0]; // previous house
-        for(int i = 1; i<n; i++) {
-            int steal = nums[i] + prev2;
-            int leave = prev1;
-            prev2 = prev1;
-            prev1 = max(steal,leave);
-        }
-        return prev1;
+        n = nums.size();
+        vector<int> dp (n,-1);
+        return solve(nums,0,dp);
     }
 };
