a1d2fad56f8d9effbb00586e34e6585590c27372
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-08-12 21:05:48 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (74.91%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
-    int solve (vector<int> &nums, int start, int end) {
-        int prev2 = 0;
-        int prev1 = nums[start];
-        for(int i = start+1; i<end; i++) {
-            int steal = prev2 + nums[i];
-            int leave = prev1;
-            int temp = max(steal,leave);
-            prev2 = prev1;
-            prev1 = temp;
-        }
-        return prev1;
-    }
     int rob(vector<int>& nums) {
-        if(nums.size() == 1) return nums[0];
-        return max(solve(nums,0,nums.size()-1),solve(nums,1,nums.size()));
+        int n = nums.size();
+
+        if(n == 1) return nums[0];
+
+        int prev1 = 0;
+        int prev2 = 0;
+
+        for(int i = n-1; i> 0; i--) {
+            int take = nums[i] + prev2;
+            int skip = prev1;
+
+            prev2 = prev1;
+            prev1 = max(skip,take);
+        }
+
+        int prev3 = 0;
+        int prev4 = 0;
+
+        for(int i = n-2; i>= 0; i--) {
+            int take = nums[i] + prev4;
+            int skip = prev3;
+
+            prev4 = prev3;
+            prev3 = max(skip,take);
+        }
+
+        return max(prev1,prev3);
     }
 };
