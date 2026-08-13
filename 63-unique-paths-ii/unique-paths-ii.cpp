3270c7f7ff50af0bb62019a98d007bd22a9142a9
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-08-13 19:38:06 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (32.32%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -17,11 +17,11 @@ public:
                     continue;
                 }
 
-                int up = 0, right = 0;
+                int up = 0, left = 0;
                 if(i > 0) up = dp[i-1][j];
-                if(j > 0) right = dp[i][j-1];
+                if(j > 0) left = dp[i][j-1];
 
-                dp[i][j] = up + right;
+                dp[i][j] = up + left;
             }
         }
 
