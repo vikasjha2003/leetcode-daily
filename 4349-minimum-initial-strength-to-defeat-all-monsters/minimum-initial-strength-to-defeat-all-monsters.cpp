39cbefed695f93db7b0eecb3d8b37dcde64fb662
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-08-15 11:13:27 +0530

    Time: 11 ms (84.20%) | Memory: 243.4 MB (86.04%) - LeetSync

--- 4349-minimum-initial-strength-to-defeat-all-monsters/minimum-initial-strength-to-defeat-all-monsters.cpp
+++ 4349-minimum-initial-strength-to-defeat-all-monsters/minimum-initial-strength-to-defeat-all-monsters.cpp
@@ -4,26 +4,19 @@ public:
         int n = monsters.size();
 
         vector<long long> da (n,0);
-        for(int i = 0; i<boosts.size(); i++) {
-            da[boosts[i][0]] += boosts[i][2];
-            if(boosts[i][1] + 1 < n) da[boosts[i][1] +1] -= boosts[i][2];
-        }
-
-        int idx = -1;
-        for(int i = 0; i<n; i++) {
-            if(i > 0) da[i] += da[i-1];
-            if(monsters[i] > da[i]) {
-                idx = i;
-            }
+        for(auto &b : boosts) {
+            da[b[0]] += b[2];
+            if(b[1] + 1 < n) da[b[1] +1] -= b[2];
         }
 
         long long res = 0;
-
-        for(int i = 0; i<=idx; i++) {
-            if(i == idx) {
-                res += monsters[i] - da[i];
-            } else {
-                res += monsters[i];
+        long long sum = 0;
+        long long bonus = 0;
+        for(int i = 0; i<n; i++) {
+            bonus += da[i];
+            sum += monsters[i];
+            if(monsters[i] > bonus) {
+                res = sum - bonus;
             }
         }
 
