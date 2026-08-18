c70e6138cef17c697f59ad96c3cf42933ebb7168
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-08-18 20:21:54 +0530

    Time: 10 ms (61.04%) | Memory: 337.3 MB (37.91%) - LeetSync

--- 4358-count-dominant-nodes-in-a-binary-tree/count-dominant-nodes-in-a-binary-tree.cpp
+++ 4358-count-dominant-nodes-in-a-binary-tree/count-dominant-nodes-in-a-binary-tree.cpp
@@ -2,10 +2,6 @@ class Solution {
 public:
     int solve(TreeNode* root, int &res) {
         if(!root) return 0;
-        if(!root->left && !root->right) {
-            res++;
-            return root->val;
-        }
 
         int lowerMax = max(solve(root->left,res),solve(root->right,res));
         if(root->val >= lowerMax) res++;
