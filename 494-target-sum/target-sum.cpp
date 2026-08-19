f02c272dc20da6a2cdf8ff11c35eecb3050bcd17
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-08-19 23:26:58 +0530

    Time: 4 ms (77.12%) | Memory: 15.5 MB (45.31%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    int n;
-    int solve(vector<int> &nums, int idx, int target, vector<vector<int>> &dp) {
-        if(idx == n) {
-            if(target == 0) return 1;
-            return 0;
-        } 
-        if(dp[idx][target] != -1) return dp[idx][target];
-
-        int skip = solve(nums,idx+1,target,dp);
-        int take = 0;
-        if(nums[idx] <= target) {
-            take = solve(nums,idx+1,target - nums[idx],dp);
-        }
-
-        return dp[idx][target] = skip + take;
-    }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        n = nums.size();
+        int n = nums.size();
 
         int sum = accumulate(nums.begin(),nums.end(),0);
         if(sum < target || (sum - target) % 2 == 1) return 0;
 
-        vector<vector<int>> dp (n,vector<int> ((sum-target)/2+1,-1));
-
-        return solve(nums,0,(sum-target)/2,dp);
+        vector<vector<int>> dp (n+1,vector<int> ((sum-target)/2+1,0));
+        dp[n][0] = 1;
+        
+        for(int i = n-1; i>= 0; i--) {
+            for(int j = (sum-target)/2; j >= 0; j--) {
+                int skip = dp[i+1][j];
+                int take = 0;
+                if(nums[i] <= j) {
+                    take = dp[i+1][j-nums[i]];
+                }
+                
+                dp[i][j] = skip + take;
+            }
+        }
+        
+        return dp[0][(sum-target)/2];        
     }
 };
