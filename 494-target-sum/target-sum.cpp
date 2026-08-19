edf9402f751bf114a6efcbe07ef61b10fa9949db
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-08-19 23:28:27 +0530

    Time: 2 ms (88.32%) | Memory: 12.8 MB (63.56%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -6,21 +6,21 @@ public:
         int sum = accumulate(nums.begin(),nums.end(),0);
         if(sum < target || (sum - target) % 2 == 1) return 0;
 
-        vector<vector<int>> dp (n+1,vector<int> ((sum-target)/2+1,0));
-        dp[n][0] = 1;
+        vector<int> dp ((sum-target)/2+1,0);
+        dp[0] = 1;
         
         for(int i = n-1; i>= 0; i--) {
             for(int j = (sum-target)/2; j >= 0; j--) {
-                int skip = dp[i+1][j];
+                int skip = dp[j];
                 int take = 0;
                 if(nums[i] <= j) {
-                    take = dp[i+1][j-nums[i]];
+                    take = dp[j-nums[i]];
                 }
                 
-                dp[i][j] = skip + take;
+                dp[j] = skip + take;
             }
         }
         
-        return dp[0][(sum-target)/2];        
+        return dp[(sum-target)/2];        
     }
 };
