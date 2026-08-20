aa11640c6972ffa98369d9bf38b94ef6aeba3496
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-08-20 22:49:55 +0530

    Time: 62 ms (51.25%) | Memory: 13.1 MB (85.32%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     int numSquares(int n) {
         int m = sqrt(n);
-        vector<vector<int>> dp (m+2,vector<int> (n+1,INT_MAX));
-        for(int i = 0; i<=m+1; i++) {
-            dp[i][0] = 0;
-        }
+        vector<int> dp (n+1,INT_MAX);
+        dp[0] = 0;
 
         for(int i = m; i> 0; i--) {
             for(int j = 1; j <= n; j++) {
-                int skip = dp[i+1][j];
+                int skip = dp[j];
                 int take = INT_MAX;
                 if(i*i <= j) {
-                    if(dp[i][j-i*i] != INT_MAX) take = 1 + dp[i][j-i*i];
+                    if(dp[j-i*i] != INT_MAX) take = 1 + dp[j-i*i];
                 }
 
-                dp[i][j] = min(skip,take);
+                dp[j] = min(skip,take);
             }
         }
 
-        return dp[1][n];
+        return dp[n];
     }
 };
