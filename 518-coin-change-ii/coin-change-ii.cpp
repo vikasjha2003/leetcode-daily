dc6980efc6a068b46ab33361a6919e6fbb27f014
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-08-20 19:50:03 +0530

    Time: 46 ms (26.20%) | Memory: 36.7 MB (39.89%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-
     int n;
-    int solve(vector<int>& coins, int amount, int idx, vector<vector<int>> &dp) {
-        if(amount == 0) return 1;
+
+    int solve(vector<int> &coins, int idx, int target, vector<vector<int>> &dp) {
+        if(target == 0) return 1;
         if(idx == n) return 0;
-        if(dp[idx][amount] != -1) return dp[idx][amount];
-
-        int skip = solve(coins,amount,idx+1,dp);
-
+        if(dp[idx][target] != -1) return dp[idx][target];
+        
+        int skip = solve(coins,idx+1,target,dp);
         int take = 0;
-
-        if(coins[idx] <= amount) {
-            take = solve(coins,amount - coins[idx],idx,dp);
+        if(coins[idx] <= target) {
+            take = solve(coins,idx,target - coins[idx],dp);
         }
-
-        return dp[idx][amount] = skip + take;
+        
+        return dp[idx][target] = skip + take;
     }
 
     int change(int amount, vector<int>& coins) {
         n = coins.size();
         vector<vector<int>> dp (n, vector<int> (amount+1,-1));
-        return solve(coins,amount,0,dp);
+        return solve(coins,0,amount,dp);
     }
 };
