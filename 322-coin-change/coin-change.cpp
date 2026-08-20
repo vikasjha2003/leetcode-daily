155d023b572f6681419f50f1d3335ee05e6a5d2d
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-08-20 22:18:13 +0530

    Time: 63 ms (10.70%) | Memory: 53 MB (13.57%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    int n;
-    int solve(vector<int>& coins, int amount, int idx, vector<vector<int>> &dp) {
-        if(amount == 0) return 0;
-        if(idx == n) return INT_MAX;
-        if(dp[idx][amount] != -1) return dp[idx][amount];
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
 
-        int skip = solve(coins,amount,idx+1,dp);
-        int take = INT_MAX;
-        if(coins[idx] <= amount) {
-            int res = solve(coins,amount-coins[idx],idx,dp);
-            if(res != take) {
-                take = 1 + res;
+        vector<vector<int>> dp (n+1, vector<int> (amount+1,INT_MAX));
+        for(int i = 0; i<=n; i++) {
+            dp[i][0] = 0;
+        }
+
+        for(int i = n-1; i>=0; i--) {
+            for(int j = 1; j<=amount; j++) {
+                int skip = dp[i+1][j];
+                int take = INT_MAX;
+                if(coins[i] <= j) {
+                    if(dp[i][j-coins[i]] != take) {
+                        take = 1 + dp[i][j-coins[i]];
+                    }
+                }
+
+                dp[i][j] = min(skip,take);
             }
         }
 
-        return dp[idx][amount] = min(skip,take);
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        n = coins.size();
-        vector<vector<int>> dp (n, vector<int> (amount+1,-1));
-        int result = solve(coins,amount,0,dp);
-        if(result < INT_MAX) return result;
+        if(dp[0][amount] < INT_MAX) return dp[0][amount];
         return -1;
     }
 };
