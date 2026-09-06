2cbabea5fecb980e9acdb283644a35848d5d103c
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-09-06 11:43:11 +0530

    Time: 11 ms (87.61%) | Memory: 8.5 MB (99.88%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -4,19 +4,17 @@ public:
         int n = s.length();
         int m = t.length();
 
-        vector<vector<int>> dp (n+1, vector<int> (m+1,0));
-        for(int i = 0; i<=n; i++) {
-            dp[i][m] = 1;
-        }
+        vector<int> dp (m+1,0);
+        dp[m] = 1;
 
         for(int i = n-1; i>= 0; i--) {
-            for(int j = m-1; j>=0; j--) {
-                dp[i][j] = dp[i+1][j];
+            for(int j = 0; j<m; j++) {
+                dp[j] = dp[j];
                 if(s[i] == t[j]) {
-                    dp[i][j] = (long long)dp[i][j] + dp[i+1][j+1];
+                    dp[j] = (long long)dp[j] + dp[j+1];
                 }
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
