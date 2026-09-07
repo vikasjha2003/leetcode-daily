0dc2300ab9a874e8d359699f58545e576e6d3621
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-09-07 22:01:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (43.73%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    long mod = 1e9 + 7;
-    int solve(string &s, int n, vector<int> &dp, vector<int> &lastIdx) {
-        if(n == 0) return 0;
-        if(dp[n] != -1) return dp[n];
-        
-        int prev = solve(s,n-1,dp,lastIdx);
-
-        int duplicate = lastIdx[s[n-1]-'a'];
-
-        int total = ((2 * prev +1) % mod - duplicate + mod) % mod;
-
-        lastIdx[s[n-1]-'a'] = prev + 1;
-
-        return dp[n] = total;       
-    }
-
+    const long mod = 1e9 + 7;
     int distinctSubseqII(string s) {
         int n = s.length();
-        vector<int> dp (n+1,-1);
+
+        vector<int> dp (n+1,0);
         vector<int> lastIdx (26,0);
-        return solve(s,n,dp,lastIdx);
+
+        for(int i = 1; i<=n; i++) {
+            dp[i] = (2 * (long long)(dp[i-1]) + 1) % mod;
+            dp[i] = (dp[i] - lastIdx[s[i-1] - 'a'] + mod) % mod;
+            lastIdx[s[i-1] - 'a'] = dp[i-1] + 1;
+        }
+
+        return dp[n];
     }
 };
