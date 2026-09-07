e341b5c43711a4d81e211b85d2a77532683eee07
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-09-07 22:05:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (98.64%) - LeetSync

--- 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
+++ 977-distinct-subsequences-ii/distinct-subsequences-ii.cpp
@@ -4,15 +4,16 @@ public:
     int distinctSubseqII(string s) {
         int n = s.length();
 
-        vector<int> dp (n+1,0);
         vector<int> lastIdx (26,0);
+        int prev = 0;
 
         for(int i = 1; i<=n; i++) {
-            dp[i] = (2 * (long long)(dp[i-1]) + 1) % mod;
-            dp[i] = (dp[i] - lastIdx[s[i-1] - 'a'] + mod) % mod;
-            lastIdx[s[i-1] - 'a'] = dp[i-1] + 1;
+            int temp = (2 * (long long)prev + 1) % mod;
+            temp = (temp - lastIdx[s[i-1] - 'a'] + mod) % mod;
+            lastIdx[s[i-1] - 'a'] = prev + 1;
+            prev = temp;
         }
 
-        return dp[n];
+        return prev;
     }
 };
