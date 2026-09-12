50762781ca5a23c4336a949eb9f9ec86cda2af7c
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-09-12 17:03:04 +0530

    Time: 6 ms (48.00%) | Memory: 107.7 MB (15.74%) - LeetSync

---
+++ 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int waysToMakeFair(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> suffixO (n+1,0);
+        vector<int> suffixE (n+1,0);
+        int sumo = 0, sume = 0;
+
+        for(int i = n-1; i>= 0; i--) {
+            if(i % 2 == 0) sume += nums[i];
+            else sumo += nums[i];
+
+            suffixE[i] += sume;
+            suffixO[i] += sumo;
+        }
+
+        int ways = 0;
+
+        sumo = 0;
+        sume = 0;
+
+        for(int i = 0; i<n; i++) {
+            if(sume + suffixO[i+1] == sumo + suffixE[i+1]) ways++;
+
+            if(i%2 == 0) sume += nums[i];
+            else sumo += nums[i];
+        }
+        
+        return ways;
+    }
+};
