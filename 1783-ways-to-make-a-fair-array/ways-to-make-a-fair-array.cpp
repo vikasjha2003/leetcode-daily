6be0e4b68c35b09ff5e967d504b7a327538806bf
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-09-12 17:10:32 +0530

    Time: 2 ms (76.17%) | Memory: 98.3 MB (42.53%) - LeetSync

--- 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.cpp
+++ 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.cpp
@@ -3,28 +3,26 @@ public:
     int waysToMakeFair(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> suffixO (n+1,0);
-        vector<int> suffixE (n+1,0);
-        int sumo = 0, sume = 0;
+        int sufo = 0, sufe = 0;
 
         for(int i = n-1; i>= 0; i--) {
-            if(i%2==0) sume += nums[i];
-            else sumo += nums[i];
-
-            suffixE[i] += sume;
-            suffixO[i] += sumo;
+            if(i%2==0) sufe += nums[i];
+            else sufo += nums[i];
         }
 
         int ways = 0;
 
-        sumo = 0;
-        sume = 0;
+        int prefo = 0;
+        int prefe = 0;
 
         for(int i = 0; i<n; i++) {
-            if(sume + suffixO[i+1] == sumo + suffixE[i+1]) ways++;
+            if(i%2==0) sufe -= nums[i];
+            else sufo -= nums[i];
 
-            if(i%2==0) sume += nums[i];
-            else sumo += nums[i];
+            if(prefe + sufo == prefo + sufe) ways++;
+
+            if(i%2==0) prefe += nums[i];
+            else prefo += nums[i];
         }
         
         return ways;
