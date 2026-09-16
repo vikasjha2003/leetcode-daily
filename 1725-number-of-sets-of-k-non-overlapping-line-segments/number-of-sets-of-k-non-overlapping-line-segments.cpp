a342fc003399fd368bef74a3fe66ebab08bde09e
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-09-16 14:13:03 +0530

    Time: 2531 ms (5.14%) | Memory: 32.2 MB (44.86%) - LeetSync

---
+++ 1725-number-of-sets-of-k-non-overlapping-line-segments/number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    const long mod = 1e9 + 7;
+    int m;
+    int solve (int n, int k, vector<vector<int>> &dp) {
+        if(n < 1) return 0;
+        if(k == 0) return 1;
+        if(dp[n][k] != -1) return dp[n][k];
+
+        long long result = solve(n-1,k,dp);
+        for(int i = 1; i <= m; i++) {
+            result = (result + solve(n-i,k-1,dp)) % mod;
+        }
+
+        return dp[n][k] = result;
+    }
+
+    int numberOfSets(int n, int k) {
+        m = n - k;
+        vector<vector<int>> dp (n+1, vector<int> (k+1,-1));
+        return solve(n,k,dp);
+    }
+};
