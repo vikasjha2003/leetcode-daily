3768985b8b4f70170cb823e4948b90ae3fb190a8
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-09-19 23:22:25 +0530

    Time: 0 ms (100.00%) | Memory: 79.3 MB (87.77%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -3,21 +3,21 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
 
-        int dp[n+1][3][2];
+        int dp[3][2];
         memset(dp,0,sizeof(dp));
 
         for(int i = n-1; i>=0; i--) {
             for(int j = 1; j<=2; j++) {
                 for(int k = 0; k<=1; k++) {
                     if(k) {
-                        dp[i][j][k] = max(-prices[i]+dp[i+1][j][!k],dp[i+1][j][k]);
+                        dp[j][k] = max(-prices[i]+dp[j][!k],dp[j][k]);
                     } else {
-                        dp[i][j][k] = max(prices[i]+dp[i+1][j-1][!k],dp[i+1][j][k]);
+                        dp[j][k] = max(prices[i]+dp[j-1][!k],dp[j][k]);
                     }
                 }
             }
         }
 
-        return dp[0][2][1];
+        return dp[2][1];
     }
 };
