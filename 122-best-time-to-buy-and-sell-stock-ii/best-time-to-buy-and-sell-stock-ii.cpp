d056446ef45e10baacef99408871bb47439369a0
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-09-19 22:53:07 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (43.08%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,16 +3,16 @@ public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
 
-        vector<vector<int>> dp;
-        dp.resize(n+1,vector<int> (2,0));
+        vector<int> dp;
+        dp.resize(2,0);
 
         for(int i = n-1; i>=0; i--) {
             for(int j = 0; j<2; j++) {
-                if(j) dp[i][j] = max(-prices[i]+dp[i+1][!j],dp[i+1][j]);
-                else dp[i][j] = max(prices[i]+dp[i+1][!j],dp[i+1][j]);
+                if(j) dp[j] = max(-prices[i]+dp[!j],dp[j]);
+                else dp[j] = max(prices[i]+dp[!j],dp[j]);
             }
         }
 
-        return dp[0][1];
+        return dp[1];
     }
 };
