85c1b708e264837fc31e490eedf6fdee27a4f41b
Vikas Kumar Jha <vikas2003jha@gmail.com>
2026-09-24 12:19:02 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (89.99%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.cpp
+++ 1242-matrix-block-sum/matrix-block-sum.cpp
@@ -2,18 +2,27 @@ class Solution {
 public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
         int m = mat.size(), n = mat[0].size();
+
+        for(int i = 0; i<m; i++) {
+            int sum = 0;
+            for(int j = 0; j<n; j++) {
+                sum += mat[i][j];
+                if(i == 0) mat[i][j] = sum;
+                else mat[i][j] = sum + mat[i-1][j];
+            }
+        }
+
         vector<vector<int>> res (m,vector<int> (n));
+
         for(int i = 0; i<m; i++) {
             for(int j = 0; j<n; j++) {
-                int sum = 0;
-                for(int r = i-k; r <= i+k; r++) {
-                    if(r < 0 || r >= m) continue;
-                    for(int c = j-k; c <= j+k; c++) {
-                        if(c < 0 || c >= n) continue;
-                        sum += mat[r][c];
-                    }
-                }
-                res[i][j] = sum;
+                int row1 = max(0,i-k), col1 = max(0,j-k);
+                int row2 = min(m-1,i+k), col2 = min(n-1,j+k);
+
+                if(row1 == 0 && col1 == 0) res[i][j] = mat[row2][col2];
+                else if (row1 == 0) res[i][j] = mat[row2][col2] - mat[row2][col1-1];
+                else if (col1 == 0) res[i][j] = mat[row2][col2] - mat[row1-1][col2];
+                else res[i][j] = mat[row2][col2] + mat[row1-1][col1-1] - mat[row2][col1-1] - mat[row1-1][col2];
             }
         }
 
